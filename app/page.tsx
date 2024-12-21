1bca36eb3c47bdcdb3005cd542acc5a36ef2b944
Itsuki0016 <167990332+Itsuki0016@users.noreply.github.com>
2024-12-21 15:01:18 +0900

    change portfolio

---
+++ app/page.tsx
@@ -0,0 +1,40 @@
+import styles from "./page.module.css";
+import Image from "next/image";
+import { TOP_NEWS_LIMIT } from '@/app/constants'
+import { getNewsList } from '@/app/libs/microcms'
+import NewsList from "@/app/_components/NewsList";
+import ButtonLink from "@/app/_components/ButtonLink";
+
+
+
+
+
+export default async function Home(){
+  const data = await getNewsList({limit:TOP_NEWS_LIMIT,});
+
+  return(
+    <>
+      <section className={styles.top}>
+        <div>
+          <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+          <p className={styles.decriptiobn}>私たちは市場をリードしているグローバルテックカンパニーです。</p>
+        </div>
+        <Image
+        className = {styles.bgimg}
+        src = "/img-mv.jpg"
+        alt = ""
+        width = {4000}
+        height = {1200}
+        />  
+      </section>
+
+      <section className={styles.news}> 
+        <h2 className={styles.newstitle}>News</h2>
+        <NewsList news={data.contents} />
+        <div className={styles.newsLink}>
+          <ButtonLink href="/news">もっと見る</ButtonLink>
+        </div>
+      </section>
+    </>
+  );
+}
