4ff8ab82a10c18c578cb54ff25b13a6276f79bd1
Itsuki0016 <167990332+Itsuki0016@users.noreply.github.com>
2025-01-11 15:27:25 +0900

    miss commit

--- app/_components/Portfolio.tsx
+++ app/_components/Portfolio.tsx
@@ -4,8 +4,6 @@ const Portfolio = () => (
     <section id="portfolio">
         <h2>Portfolio</h2>
         <div>
-        <Image src="/project1.jpg" alt="Project 1" width={300} height={200} />
-        <Image src="/project2.jpg" alt="Project 2" width={300} height={200} />
         </div>
     </section>
 );
