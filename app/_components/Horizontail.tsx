498bed89d2c6bfabd0f668cf801fbea30dba9aa9
Itsuki0016 <167990332+Itsuki0016@users.noreply.github.com>
2025-01-18 14:33:03 +0900

    create AboutMe

--- app/_components/Horizontail.tsx
+++ app/_components/Horizontail.tsx
@@ -6,27 +6,34 @@ function HorizontalLayout() {
             <div className="horizontal-scroll-container">
             <div className="scroll-section">
                 <div className="circuit-bg"></div>
-                <h1>Welcome</h1>
+                <h1>thank you for reading</h1>
             </div>
             <div className="scroll-section">
                 <div className="circuit-bg"></div>
-                <h1>About Me</h1>
+                <h1>Introduction</h1>
+                <p>このポートフォリオを読んでいただきありがとうございます。<br/>
+                    私は村田一生と申します。京都デザイン＆テクノロジーの学生です。
+                    このポートフォリオでは私のスキルやポートフォリオを紹介しています。
+                    <br />どうぞよろしくお願いいたします。
+                </p>
             </div>
             <div className="scroll-section">
                 <div className="circuit-bg"></div>
                 <h1>Skills</h1>
+                <p>私の扱うことのできる言語は、Python<br/>JavaScript（react,tailwind）<br/>HTML、CSS</p>
             </div>
             <div className="scroll-section">
                 <div className="circuit-bg"></div>
-                <h1>Portfolio</h1>
+                <h1>Now</h1>
+                <p>現在は一年生の進級制作展We areに向けてクラスメイトと合同でプロジェクトを進めています。</p>
             </div>
             <div className="scroll-section">
                 <div className="circuit-bg"></div>
-                <h1>Contact</h1>
+                <h1>Future</h1>
+                <p>二年次からはAWSなどの開発に特化した企業プロジェクトに積極的に参加したいです。</p>
             </div>
             </div>
         );
     }
     
     export default HorizontalLayout;
-    
--- app/_components/Portfolio.tsx
+++ app/_components/Portfolio.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Portfolio = () => (
     <section id="portfolio">
-        <h2>Portfolio</h2>
+        <h2>Article</h2>
         <div>
         </div>
     </section>
