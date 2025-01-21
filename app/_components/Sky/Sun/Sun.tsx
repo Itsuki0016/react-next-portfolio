9eccdd27f64c9d820cfdbba10c86529c829a0869
Itsuki0016 <167990332+Itsuki0016@users.noreply.github.com>
2025-01-22 07:13:46 +0900

    Sun add airplane

--- app/_components/Sky/Sun/Sun.tsx
+++ app/_components/Sky/Sun/Sun.tsx
@@ -7,9 +7,13 @@ const Sun: React.FC = () => {
     return (
         <div className="sun-container">
             <div className="sun"></div>
-            <div className="cloud-1"></div>
-            <div className="cloud-2"></div>
-            <div className="cloud-3"></div>
+            <div className="cloud cloud-1"></div>
+            <div className="cloud cloud-2"></div>
+            <div className="cloud cloud-3"></div>
+            <div className="airplane-container">
+                <div className="airplane"></div>
+                <div className="contrail"></div>
+            </div>
         </div>
     );
 };
